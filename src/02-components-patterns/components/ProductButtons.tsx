eae719c4d067a90e284b276c81010a1c1a6446b6
Daniel Arista <arista.dan18@gmail.com>
2022-02-06 16:50:14 -0600

    Component Extend Styles - listo

--- src/02-components-patterns/components/ProductButtons.tsx
+++ src/02-components-patterns/components/ProductButtons.tsx
@@ -2,15 +2,22 @@ import { useContext } from "react";
 import { ProductContext } from "./ProductCard";
 import styles from "../styles/styles.module.css";
 
-export const ProductButtons = () => {
+interface Props {
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export const ProductButtons = ({ className, style }: Props) => {
   const { increaseBy, counter } = useContext(ProductContext);
 
   return (
-    <div className={styles.buttonsContainer}>
+    <div className={`${styles.buttonsContainer} ${className}`} style={style}>
       <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
         -
       </button>
-      <div className={styles.countLabel}>{counter}</div>
+      <div className={styles.countLabel}>
+        <span>{counter}</span>
+      </div>
       <button className={styles.buttonAdd} onClick={() => increaseBy(1)}>
         +
       </button>
--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -4,6 +4,7 @@ import {
   ProductImage,
   ProductTitle,
 } from "../components/index";
+import "../styles/custom-styles.css";
 
 const product = {
   id: "1",
@@ -16,16 +17,26 @@ export const ShoppingPage = () => {
     <div>
       <h1 className="text-3xl">Shopping Store</h1>
       <hr />
+
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        <ProductCard product={product}>
-          <ProductCard.Image />
-          <ProductCard.Title />
-          <ProductCard.Buttons />
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductCard.Image className="custom-image" />
+          <ProductCard.Title className="text-xl transition-all hover:underline cursor-pointer" />
+          <ProductCard.Buttons className="custom-buttons" />
         </ProductCard>
-        <ProductCard product={product}>
-          <ProductImage />
-          <ProductTitle title="Azul" />
-          <ProductButtons />
+
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductImage className="custom-image" />
+          <ProductTitle className="text-xl transition-all hover:underline cursor-pointer" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard product={product} style={{ backgroundColor: "#70d1f8" }}>
+          <ProductImage style={{ borderRadius: "1ppx", padding: "10px" }} />
+          <ProductTitle style={{ fontWeight: "bold" }} />
+          <ProductButtons
+            style={{ display: "flex", justifyContent: "center" }}
+          />
         </ProductCard>
       </div>
     </div>
